a371074cd11eb167313ce70ee2d0489d9e02ddc5
1996Woso <eviwe.khohlombe@gmail.com>
2024-06-26 17:15:39 +0200

    added calculation of other variables

--- src/app/user-input/user-input.component.ts
+++ src/app/user-input/user-input.component.ts
@@ -36,11 +36,73 @@ export class UserInputComponent {
         accumulatedAmount / (1 + (interestRate / 100) * period)
       ).toFixed(2);
     }
+    else if(interestRate&&interestRate&&period){
+      this.userPrincipal = (100*(interestEarned/(interestRate*period))).toFixed(2);
+    }
     else if(accumulatedAmount && interestEarned){
       this.userPrincipal = (accumulatedAmount-interestEarned).toFixed(2);
     } else {
       alert('Please enter values for Accumulated Amount, Interest Rate, and Period.');
     }
-    
+  }
+  
+  calculateAmaount(){
+    const principal = +this.userPrincipal;
+    const interestRate = +this.userRate;
+    const period = +this.userPeriod;
+    const interestEarned = +this.userInterest;
+    if(principal&&interestRate&&period){
+      this.userAmount = (principal*(1+(interestRate/100)*period)).toFixed(2);
+    }
+    else if(principal&&interestEarned){
+      this.userAmount = (interestEarned+principal).toFixed(2);
+    }else{
+      alert('Please fill in required fields correctly.');
+    }
+  }
+
+  calculatePeriod(){
+    const principal = +this.userPrincipal;
+    const interestRate = +this.userRate;
+    const accumulatedAmount = +this.userAmount;
+    const interestEarned = +this.userInterest;
+    if(principal&&interestRate&&accumulatedAmount){
+      this.userPeriod = (((accumulatedAmount/principal)-1)/(interestRate/100)).toFixed(2);
+    }
+    else if(interestEarned&&principal&&interestRate){
+      this.userRate = (100*(interestEarned/(principal*interestRate))).toFixed(2);
+    }else{
+      alert('Please fill in required fields correctly.');
+    }
+  }
+
+  calculateRate(){
+    const principal = +this.userPrincipal;
+    const period = +this.userRate;
+    const accumulatedAmount = +this.userAmount;
+    const interestEarned = +this.userInterest;
+    if(principal&&period&&accumulatedAmount){
+      this.userRate = (100*((accumulatedAmount/principal)-1)/period).toFixed(2);
+    }
+    else if(interestEarned&&principal&&period){
+      this.userRate = (100*(interestEarned/(principal*period))).toFixed(2);
+    }else{
+      alert('Please fill in required fields correctly.');
+    }
+  }
+
+  calculateInterest(){
+    const principal = +this.userPrincipal;
+    const period = +this.userRate;
+    const accumulatedAmount = +this.userAmount;
+    const interestRate = +this.userRate;
+    if(principal&&interestRate&&period){
+      this.userInterest = ((principal*period*interestRate)/100).toFixed(2);
+    }
+    else if(principal&& accumulatedAmount){
+      this.userInterest = (accumulatedAmount-principal).toFixed(2);
+    }else{
+      alert('Please fill in required fields correctly.');
+    }
   }
 }
